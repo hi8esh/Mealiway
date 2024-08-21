5acddb574ba107625701dfe9c6760546aa0f3ddd
hi8esh <maheshsudha56@gmail.com>
2024-08-21 08:35:52 +0530

    Completed Home Page Setup

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,9 @@ const FoodDisplay = ({category}) => {
       <h2>Top Dishes near your</h2>
       <div className="food-display-list">
         {food_list.map((item, index)=>{
-            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price}  image={item.image}/>
+            if(category==="All" || category===item.category){
+              return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price}  image={item.image}/>
+            }
         })}
       </div>
     </div>
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+91-97283875643</li>
+                    <li>contact@mealiway.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">Copyright 2024 @ Mealiway.com - All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
